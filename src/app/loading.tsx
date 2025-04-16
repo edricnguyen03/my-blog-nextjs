3fc085320391c440199e5da0fb399983340c210d
Edric <datnguyen2132003@gmail.com>
2025-04-17 01:44:50 +0700

    update v3

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,8 @@
 export default function Loading() {
-    return <div>Loading...</div>;
+    return (
+        <div className="flex items-center text-center justify-center h-screen">
+            <h1 className="text-2xl font-bold">Loading...</h1>
+            <p className="text-lg">Please wait while we load your content.</p>
+        </div>
+    );
 }
